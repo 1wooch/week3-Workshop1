68a4125a15679a3ec24f93e0d93ea7c27bb67d86
wonwo choi <onechoo1106@gmail.com>
2023-03-21 16:20:41 +1000

    Initial Commit

---
+++ week3 Workshop/week3_WorkshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  week3_WorkshopApp.swift
+//  week3 Workshop
+//
+//  Created by Wonwoo Choi on 21/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct week3_WorkshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
